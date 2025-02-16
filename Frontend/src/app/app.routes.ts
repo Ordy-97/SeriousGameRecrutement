18721bd7a99f2cf17e62348d76e5bcbd8da6d2a2
GHEMNING Aaron <aaron.ghemning@institutsaintjean.com>
2025-02-16 15:26:06 +0100

    Feat: Implement admin login component with authentication service and routing

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AdminLoginComponent } from './admin/admin-login/admin-login.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'admin',
+    children: [{ path: 'login', component: AdminLoginComponent }],
+  },
+];
